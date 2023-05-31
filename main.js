63eb177d131ea900f44672b0a89bc578429a6c3f
aasscc07 <75631159+aasscc07@users.noreply.github.com>
2023-06-01 04:30:20 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const SonOfMainDiv = document.getElementById('Whole')
+
+
+SonOfMainDiv.addEventListener('mouseup',(e) => {
+    SonOfMainDiv.style.color = "blue"
+})
+
+SonOfMainDiv.addEventListener('mousedown',(e) => {
+    SonOfMainDiv.style.color = "white"
+})
+
