c0921fedf05fb994993097b33f61871242b5265e
aasscc07 <75631159+aasscc07@users.noreply.github.com>
2023-06-04 21:55:30 +0900

    Add files via upload

---
+++ jinjin/main.js
@@ -0,0 +1,17 @@
+
+
+window.onload = function () {
+    let Project1 = document.getElementById("Project1");
+    Project1.onclick = Project1_F;
+
+}
+
+ 
+
+function Project1_F()  {
+    location.href = "Project1.html"
+}
+
+function GoMain() {
+    location.href = "index.html"
+}
